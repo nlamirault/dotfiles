3a786c7620dbc9f11fcd836dec9c4378e591c98b
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2019-01-15 12:29:49 +0100

    Add: Fish Shell configuration
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ .config/fish/config.fish
@@ -0,0 +1,27 @@
+# Copyright (C) Nicolas Lamirault <nicolas.lamirault@gmail.com>
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if not functions -q fisher
+    echo "Install Fisher"
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+source $HOME/.config/fish/plugins.fish
+
+# Load local configuration
+if test -e $HOME/.local.fish
+    source $HOME/.local.fish
+end
