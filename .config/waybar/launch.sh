b029c7ab8ae6f4bdfeeaa30e105d2fbd1ab54443
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2019-02-04 16:41:42 +0100

    Add: init Sway window manager and tools
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ .config/waybar/launch.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# Copyright (C) 2016-2019 Nicolas Lamirault <nicolas.lamirault@gmail.com>
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Terminate already running bar instances
+killall -q waybar
+
+# Wait until the processes have been shut down
+while pgrep -x waybar >/dev/null; do sleep 1; done
+
+waybar &
+echo "Bars launched..."
