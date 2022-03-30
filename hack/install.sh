c3c2625514f21ff361e53f0dc329203f5675caf6
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2022-03-30 15:13:58 +0200

    Update: Refactoring install script
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- hack/install.sh
+++ hack/install.sh
@@ -23,66 +23,78 @@ function create_link() {
 }
 
 function install_shell {
-    test -L ${HOME}/.config/shrc.d || ln -fs ${BASE_DIR}/.config/shrc.d/ ${HOME}/.config/shrc.d
-	test -L ${HOME}/.bashrc || ln -fs ${BASE_DIR}/.bashrc ${HOME}/.bashrc
-	test -L ${HOME}/.bash_profile || ln -fs ${BASE_DIR}/.bash_profile ${HOME}/.bash_profile
-	test -L ${HOME}/.config/bashrc.d || ln -fs ${BASE_DIR}/.config/bashrc.d/ ${HOME}/.config/bashrc.d
-	test -L ${HOME}/.zshrc || ln -fs ${BASE_DIR}/.zshrc ${HOME}/.zshrc
-	test -L ${HOME}/.zprofile || ln -fs ${BASE_DIR}/.zprofile ${HOME}/.zprofile
-	test -L ${HOME}/.config/zshrc.d || ln -fs ${BASE_DIR}/.config/zshrc.d/ ${HOME}/.config/zshrc.d
-	test -L ${HOME}/.config/fish || ln -fs ${BASE_DIR}/.config/fish ${HOME}/.config/fish
+	create_link ".config/shrc.d"
+	create_link ".bashrc"
+	create_link ".bash_profile"
+	create_link ".config/bashrc.d"
+	create_link ".zshrc"
+	create_link ".zprofile"
+	create_link ".config/zshrc.d"
+	create_link ".config/fish"
 }
 
 function install_binaries {
 	mkdir -p ${HOME}/bin
-	test -L ${HOME}/bin/tmux-lam.sh || ln -fs ${BASE_DIR}/bin/tmux-lam.sh ${HOME}/bin/tmux-lam.sh
-	test -L ${HOME}/bin/tmux-perso.sh || ln -fs ${BASE_DIR}/bin/tmux-perso.sh ${HOME}/bin/tmux-perso.sh
-	test -L ${HOME}/bin/tmux-zeiot.sh || ln -fs ${BASE_DIR}/bin/tmux-zeiot.sh ${HOME}/bin/tmux-zeiot.sh
-	test -L ${HOME}/bin/tmux-pilotariak.sh || ln -fs ${BASE_DIR}/bin/tmux-pilotariak.sh ${HOME}/bin/tmux-pilotariak.sh
-	test -L ${HOME}/bin/wifi-network.sh || ln -fs ${BASE_DIR}/bin/wifi-network.sh ${HOME}/bin/wifi-network.sh
+	create_link "/bin/tmux-lam.sh"
+	create_link "/bin/tmux-perso.sh"
+	create_link "/bin/tmux-zeiot.sh"
+	create_link "/bin/tmux-pilotariak.sh"
+	create_link "/bin/alacritty-perso.sh"
+	create_link "/bin/zellij.sh"
+	create_link "/bin/yk-reset.sh"
 }
 
 function install_share {
-	test -L ${HOME}/.local/share/backgrounds || ln -s ${BASE_DIR}/.local/share/backgrounds ${HOME}/.local/share/backgrounds
-	test -L ${HOME}/.local/share/man || ln -s ${BASE_DIR}/.local/share/man ${HOME}/.local/share/man
-	test -L ${HOME}/.local/share/icons || ln -s ${BASE_DIR}/.local/share/icons ${HOME}/.local/share/icons
+	create_link ".local/share/backgrounds"
+	create_link ".local/share/man"
+	create_link ".local/share/icons"
 }
 
 function install_apps {
-	test -L ${HOME}/.config/user-dirs.dirs || ln -s ${BASE_DIR}/.config/user-dirs.dirs ${HOME}/.config/user-dirs.dirs
-	test -L ${HOME}/.config/user-dirs.locale || ln -s ${BASE_DIR}/.config/user-dirs.locale ${HOME}/.config/user-dirs.locale
-	test -L ${HOME}/.config/mimeapps.list || ln -s ${BASE_DIR}/.config/mimeapps.list ${HOME}/.config/mimeapps.list
+	mkdir -p ${HOME}/.config
+	create_link ".gitconfig"
+	create_link ".gtkrc-2.0"
+	create_link ".htop"
+	create_link ".editorconfig"
+	create_link ".tmux.conf"
+	create_link ".xinitrc"
+	create_link ".config/user-dirs.dirs"
+	create_link ".config/user-dirs.locale"
+	create_link ".config/mimeapps.list"
 	create_link ".config/autostart"
 	create_link ".config/redshift"
-	test -L ${HOME}/.xinitrc || ln -fs ${BASE_DIR}/.xinitrc ${HOME}/.xinitrc
-	test -L ${HOME}/.gitconfig || ln -fs ${BASE_DIR}/.gitconfig ${HOME}/.gitconfig
-	test -L ${HOME}/.config/i3 || ln -s ${BASE_DIR}/.config/i3 ${HOME}/.config/i3
-	test -L ${HOME}/.config/conky || ln -s ${BASE_DIR}/.config/conky ${HOME}/.config/conky
-	test -L ${HOME}/.config/rofi || ln -s ${BASE_DIR}/.config/rofi ${HOME}/.config/rofi
-	test -L ${HOME}/.config/dunst || ln -s ${BASE_DIR}/.config/dunst ${HOME}/.config/dunst
-	test -L ${HOME}/.config/polybar || ln -s ${BASE_DIR}/.config/polybar ${HOME}/.config/polybar
-	# rm -f ${HOME}/.config/polybar/config && \
-	# 	ln -s ${BASE_DIR}/.config/polybar/$(POLYBAR_FILE) ${HOME}/.config/polybar/config
-	test -L ${HOME}/.config/sway || ln -s ${BASE_DIR}/.config/sway ${HOME}/.config/sway
-	test -L ${HOME}/.config/i3status-rs || ln -s ${BASE_DIR}/.config/i3status-rs ${HOME}/.config/i3status-rs
-	test -L ${HOME}/.config/tmux|| ln -s ${BASE_DIR}/.config/tmux ${HOME}/.config/tmux
-	test -L ${HOME}/.tmux.conf|| ln -s ${BASE_DIR}/.config/tmux/tmux.conf ${HOME}/.tmux.conf
-	test -L ${HOME}/.config/termite || ln -s ${BASE_DIR}/.config/termite ${HOME}/.config/termite
-	test -L ${HOME}/.config/kitty || ln -s ${BASE_DIR}/.config/termite ${HOME}/.config/kitty
-	test -L ${HOME}/.config/wal || ln -s ${BASE_DIR}/.config/wal ${HOME}/.config/wal
-	test -L ${HOME}/.gtkrc-2.0 || ln -s ${BASE_DIR}/.config/gtk/gtk2/.gtkrc-2.0 ${HOME}/.gtkrc-2.0
-	# test -L ${HOME}/.config/gtk-3.0/settings.ini || ${BASE_DIR}/.config/gtk/gtk3/settings.ini ${HOME}/.config/gtk-3.0/settings.ini
+	create_link ".config/starship.toml"
+	create_link ".config/i3"
+	create_link ".config/conky"
+	create_link ".config/rofi"
+	create_link ".config/dunst"
+	create_link ".config/polybar"
+	create_link ".config/sway"
+	create_link ".config/i3status-rs"
+	create_link ".config/tmux"
+	create_link ".config/alacritty"
+	create_link ".config/termite"
+	create_link ".config/kitty"
+	create_link ".config/wal"
+	create_link ".config/zellij"
+	mkdir -p ${HOME}/.config/Code/User
+	create_link ".config/Code/User/settings.json"
+	# create_link ""
+	# create_link ""
 }
 
 function install_ssh {
 	mkdir -p ${HOME}/.ssh
-	test -L ${HOME}/.ssh/config || ln -s ${BASE_DIR}/.ssh/config ${HOME}/.ssh/config
-	test -L ${HOME}/.ssh/personal || ln -s ${BASE_DIR}/.ssh/personal ${HOME}/.ssh/personal
+	create_link ".ssh/config"
+	create_link ".ssh/personal"
+	# test -L ${HOME}/.ssh/config || ln -s ${BASE_DIR}/.ssh/config ${HOME}/.ssh/config
+	# test -L ${HOME}/.ssh/personal || ln -s ${BASE_DIR}/.ssh/personal ${HOME}/.ssh/personal
 }
 
-# install_shell
-# install_binaries
-# install_apps
-# install_ssh
+install_shell
+install_binaries
+install_ssh
+install_share
+install_apps
 
 
