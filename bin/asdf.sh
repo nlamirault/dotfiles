9145f744ab15cccc6ca54270cdb5c6cb3f80cf01
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2023-01-08 16:36:18 +0100

    Fix: Asdf install plugins
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- bin/asdf.sh
+++ bin/asdf.sh
@@ -16,29 +16,14 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-asdf plugin-add argocd https://github.com/beardix/asdf-argocd.git
-asdf plugin-add awscli https://github.com/MetricMike/asdf-awscli.git
-asdf plugin-add boundary https://github.com/asdf-community/asdf-hashicorp.git
-# asdf plugin-add consul https://github.com/asdf-community/asdf-hashicorp.git
-# asdf plugin-add nomad https://github.com/asdf-community/asdf-hashicorp.git
-# asdf plugin-add packer https://github.com/asdf-community/asdf-hashicorp.git
-# asdf plugin-add sentinel https://github.com/asdf-community/asdf-hashicorp.git
-# asdf plugin-add serf https://github.com/asdf-community/asdf-hashicorp.git
-# asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
-asdf plugin-add vault https://github.com/asdf-community/asdf-hashicorp.git
-asdf plugin-add waypoint https://github.com/asdf-community/asdf-hashicorp.git
-asdf plugin-add conftest https://github.com/looztra/asdf-conftest
-asdf plugin-add flux2 https://github.com/tablexi/asdf-flux2.git
-asdf plugin add gcloud https://github.com/jthegedus/asdf-gcloud
-asdf plugin-add helm https://github.com/Antiarchitect/asdf-helm.git
-asdf plugin-add helm-docs https://github.com/sudermanjr/asdf-helm-docs.git
-asdf plugin add https://github.com/cgroschupp/asdf-k3sup.git
-asdf plugin-add k9s https://github.com/looztra/asdf-k9s
-asdf plugin-add kubectl https://github.com/asdf-community/asdf-kubectl.git
-asdf plugin-add linkerd https://github.com/team-gary/asdf-linkerd.git
-asdf plugin-add popeye https://github.com/nlamirault/asdf-popeye.git
-asdf plugin-add stern https://github.com/looztra/asdf-stern
-asdf plugin-add tflint https://github.com/skyzyx/asdf-tflint
-asdf plugin-add terraform-docs https://github.com/looztra/asdf-terraform-docs
-asdf plugin-add tfsec https://github.com/woneill/asdf-tfsec
-asdf plugin add starship
+case $( uname -s ) in
+    Linux)
+        cut -d' ' -f1 "${HOME}/.tool-versions" | xargs -i asdf plugin add  {}
+        ;;
+    Darwin)
+        cut -d' ' -f1 "${HOME}/.tool-versions" | xargs -I{} asdf plugin add {}
+        ;;
+    *)
+        echo "Operating system not supported"
+        ;;
+esac
